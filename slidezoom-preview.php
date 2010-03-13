8349792786280794298aeb2101f5ee0e9ddccd76
tatming <tatming@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-13 00:12:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/slidezoom/tags/1.3.2@216801 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ slidezoom-preview.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<?php
+if ( isset($_SESSION["outputhtml"]) )
+{
+$outputhtml = $_SESSION["outputhtml"];
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+	<title>SlideZoom Preview</title>
+	<?php
+	echo "<script type=\"text/javascript\" src=\"highslide/highslide.packed.js\"></script>";
+	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"highslide/highslide.css\" />";
+	echo "<script type=\"text/javascript\">";
+	echo "hs.graphicsDir=\"highslide/graphics/\";";
+	echo "hs.wrapperClassName=\"wide-border\";";
+	echo "</script>";
+	?>
+</head>
+<body>
+<?php  echo html_entity_decode($outputhtml); ?>
+</body>
+</html>
